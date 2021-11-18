b56ae90ab66fda2dfaea1b9621b0173f013476ea
Pyratite <73347888+Pyratite@users.noreply.github.com>
2021-11-18 18:47:49 +1100

    add Candy Canes

--- src/main/java/com/sh1penfire/cri_christmas/registry/ChristmasItems.java
+++ src/main/java/com/sh1penfire/cri_christmas/registry/ChristmasItems.java
@@ -1,6 +1,7 @@
 package com.sh1penfire.cri_christmas.registry;
 
 import com.sh1penfire.cri_christmas.Christmas;
+import com.sh1penfire.cri_christmas.item.ChristmasFoodComponents;
 import com.sh1penfire.cri_christmas.item.SnowballLauncherItem;
 import com.sh1penfire.cri_christmas.item.WhipItem;
 import net.minecraft.item.*;
@@ -8,17 +9,35 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ChristmasItems {
+    //slush weapons
     public static final Item WOOD_SNOWBALL_GUN = new SnowballLauncherItem(new Item.Settings().group(ItemGroup.COMBAT)){{
 
     }},
 
+    ICEBALL = new SnowballItem(new Item.Settings().group(ItemGroup.COMBAT).maxCount(16)),
+
     CANDY_CANE = new WhipItem(ToolMaterials.WOOD, 5, 1f, new Item.Settings().group(ItemGroup.COMBAT)){{}},
 
-    ICEBALL = new SnowballItem(new Item.Settings().group(ItemGroup.COMBAT).maxCount(16));
+
+    //candy foods
+
+    CANDYCANE = new Item(new Item.Settings().group(ItemGroup.FOOD).maxCount(64).food(ChristmasFoodComponents.CANDYCANE)){{
+
+    }},
+
+    CANDI = new Item(new Item.Settings().group(ItemGroup.FOOD).maxCount(64).food(ChristmasFoodComponents.CANDYCANE)){{
+
+    }};
 
     public static void load() {
         Registry.register(Registry.ITEM, new Identifier(Christmas.MOD_ID, "wood_snowball_gun"), WOOD_SNOWBALL_GUN);
         Registry.register(Registry.ITEM, new Identifier(Christmas.MOD_ID, "candy_cane"), CANDY_CANE);
         Registry.register(Registry.ITEM, new Identifier(Christmas.MOD_ID, "iceball"), ICEBALL);
+
+        //candy foods
+
+        Registry.register(Registry.ITEM, new Identifier(Christmas.MOD_ID, "candycane"), CANDYCANE);
+
+        Registry.register(Registry.ITEM, new Identifier(Christmas.MOD_ID, "candi"), CANDI);
     }
 }
